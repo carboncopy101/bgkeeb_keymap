8b0b25f3d7180eacf1432dc3c8ddac871c3a8ec6
carboncopy101 <carlo.carino@icloud.com>
2022-02-18 13:51:10 +0800

    edited qwerty thumb keys

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,--------------------.
- *                  | LOWER| Enter|   '  |    |BckSpc| Space| RAISE|
+ *                  |  '  | LOWER| ENTER |    |Space| RAISE| BckSpc|
  *                  `--------------------'    `--------------------.
  */
 
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,
   LCTL_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F), KC_G,         KC_H,    RSFT_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), RCTL_T(KC_SCLN),
   KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,
-                KC_MUTE,      LOWER,        KC_ENT,       KC_QUOT,      KC_BSPC, KC_SPC,       RAISE,        LCTL(KC_BSPC)
+                KC_MPLY,      KC_QUOT,        LOWER,       KC_ENT,      KC_SPC, RAISE,       KC_BSPC,        KC_MUTE
 ),
 
 /* Colemak
